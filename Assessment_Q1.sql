7e214cd7acd4c6373fd9e106e7ebeafb9f4ef241
James Funware West <103354487+funwarej@users.noreply.github.com>
2025-05-18 18:13:03 +0100

    Update Assessment_Q1.sql

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -1,13 +1,15 @@
 SELECT
     u.id AS owner_id,
     concat(u.first_name, ' ', u.last_name) name,
-    COUNT(CASE WHEN p.is_regular_savings = 1 and p.confirmed_amount > 0 THEN 1 END) AS savings_count,
-    COUNT(CASE WHEN p.is_a_fund = 1 and p.confirmed_amount > 0 THEN 1 END) AS investment_count,
-    SUM(p.confirmed_amount/100) AS total_deposits -- convert deposits from kobo to naira
+    SUM(p.is_regular_savings) AS savings_count,
+    SUM(p.is_a_fund) AS investment_count,
+    round(SUM(s.confirmed_amount/100),2) AS total_deposits -- convert deposits from kobo to naira
 FROM
     users_customuser u
 JOIN
     plans_plan p ON u.id = p.owner_id
+JOIN 
+	savings_savingsaccount s on s.plan_id = p.id
 GROUP BY
     u.id, u.name
 HAVING
